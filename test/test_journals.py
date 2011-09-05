8cd2ebdba88a2a324875ef662f24ed75b34a60ff
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-09-05 15:54:45 +0200

    Have testcase actually test something...

--- test/test_journals.py
+++ test/test_journals.py
@@ -4,4 +4,6 @@ from textwrap import dedent
 from resolve_citation import *
 
 def test_AA():
-    print fetch_bibtex_of_uri('doi:10.1051/0004-6361/201015906')
+    bibtex = fetch_bibtex_of_uri('doi:10.1051/0004-6361/201015906')
+    ok_('Libphst' in bibtex and 'Reinecke' in bibtex)
+
